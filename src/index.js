79b52e32b165ad4ffc03b211b871fd77c7f70a0d
Mateusz <mateusz.pytel@grupakmk.pl>
2021-01-06 18:25:33 +0100

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
+/**
+ * Plugin allows to handle multiwishlist features
+ * @param config
+ * @param db
+ * @param router
+ * @param cache
+ * @param apiStatus
+ * @param apiError
+ * @param getRestApiClient
+ * @returns {{router: *, route: string, pluginName: string, domainName: string}}
+ */
 module.exports = ({ config, db, router, cache, apiStatus, apiError, getRestApiClient }) => {
     console.warn('----> Plugin initialized');
-    
+
     return {
-        domainName: '{{domainName}}',
-        pluginName: '{{pluginName}}',
-        route: '{{restRoute}}',
+        domainName: '@grupakmk',
+        pluginName: 'multiwishlist-plugin',
+        route: '/multiwishlist',
         router
     };
 };
